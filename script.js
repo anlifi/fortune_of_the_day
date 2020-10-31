fa47909811c4a22d4fc155c251dec9f1eb4cba9a
anlifi <72402016+anlifi@users.noreply.github.com>
2020-10-31 14:23:19 +0100

    Create random message generator code

---
+++ script.js
@@ -0,0 +1,22 @@
+const messages = {
+    day: ["wonderful", "beautiful", "gloomy", "bad", "splendid", "horrible", "nice", "bright", "happy", "pleasant"],
+    todo: ["spend time with friends/family", "stay at home", "go outside", "watch Netflix", "be productive", "workout", "have some cake", "take a spa day", "read a book", "have a drink"]
+}
+
+function generateRandomIndex(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return randomIndex;
+}
+
+function generateRandomMessage() {
+    let dayMessage, todoMessage, notTodoMessage, finalMessage;
+    dayMessage = messages.day[generateRandomIndex(messages.day)];
+    todoMessage = messages.todo[generateRandomIndex(messages.todo)];
+    do {
+        notTodoMessage = messages.todo[generateRandomIndex(messages.todo)];
+    } while (notTodoMessage === todoMessage);
+    finalMessage = `Hello and welcome to ~Fortune of the Day~!\nHere's what the day brings for you:\nIt's a ${dayMessage} day. You should definitely ${todoMessage} and do not ${notTodoMessage}!`;
+    console.log(finalMessage);
+}
+
+generateRandomMessage();
